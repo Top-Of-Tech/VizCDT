4aa9356571e2d4e4d96e82457d9dc541da575aa1
Top-Of-Tech <67748521+Top-Of-Tech@users.noreply.github.com>
2021-03-13 14:13:25 -0600

    Hopefully fixed screen and animation issues

--- js/index.js
+++ js/index.js
@@ -11,18 +11,71 @@ setTimeout(function() {
 window.addEventListener("scroll", function (e) {
     scrollTop = window.pageYOffset !== undefined ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
     console.log(scrollTop);
-    if (scrollTop < 950 && scrollTop > 500) {
-        const how_to_use_text = document.querySelectorAll("#how-to-use");
-
-        for (i = 0; i < how_to_use_text.length; i++) {
-            how_to_use_text[i].classList.add("is-active");
+    if (innerHeight <= 1000) {
+        if (scrollTop < 950 && scrollTop > 500) {
+            const how_to_use_text = document.querySelectorAll("#how-to-use");
+    
+            for (i = 0; i < how_to_use_text.length; i++) {
+                how_to_use_text[i].classList.add("is-active");
+            }
+        }
+        else if (scrollTop <= 2000 && scrollTop > 1600) {
+            const about_us_text = document.querySelectorAll("#about-us");
+    
+            for (i = 0; i < about_us_text.length; i++) {
+                about_us_text[i].classList.add("is-active");
+            }
         }
     }
-    else if (scrollTop <= 1878 && scrollTop > 1378) {
-        const about_us_text = document.querySelectorAll("#about-us");
 
-        for (i = 0; i < about_us_text.length; i++) {
-            about_us_text[i].classList.add("is-active");
+    else if (innerHeight <= 900) {
+        if (scrollTop < 950 && scrollTop > 500) {
+            const how_to_use_text = document.querySelectorAll("#how-to-use");
+    
+            for (i = 0; i < how_to_use_text.length; i++) {
+                how_to_use_text[i].classList.add("is-active");
+            }
+        }
+        else if (scrollTop <= 1800 && scrollTop > 1400) {
+            const about_us_text = document.querySelectorAll("#about-us");
+    
+            for (i = 0; i < about_us_text.length; i++) {
+                about_us_text[i].classList.add("is-active");
+            }
+        }
+    }
+
+    else if (innerHeight <= 800) {
+        if (scrollTop < 950 && scrollTop > 500) {
+            const how_to_use_text = document.querySelectorAll("#how-to-use");
+    
+            for (i = 0; i < how_to_use_text.length; i++) {
+                how_to_use_text[i].classList.add("is-active");
+            }
+        }
+        else if (scrollTop <= 1600 && scrollTop > 1200) {
+            const about_us_text = document.querySelectorAll("#about-us");
+    
+            for (i = 0; i < about_us_text.length; i++) {
+                about_us_text[i].classList.add("is-active");
+            }
+        }
+    }
+
+    else if (innerHeight <= 700) {
+        if (scrollTop < 950 && scrollTop > 500) {
+            const how_to_use_text = document.querySelectorAll("#how-to-use");
+    
+            for (i = 0; i < how_to_use_text.length; i++) {
+                how_to_use_text[i].classList.add("is-active");
+            }
+        }
+        else if (scrollTop <= 1400 && scrollTop > 1000) {
+            const about_us_text = document.querySelectorAll("#about-us");
+    
+            for (i = 0; i < about_us_text.length; i++) {
+                about_us_text[i].classList.add("is-active");
+            }
         }
     }
 });
