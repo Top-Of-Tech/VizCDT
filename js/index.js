afd365bd378e20287b7a85aa3f8cc04df9b1d9ec
Top-Of-Tech <67748521+Top-Of-Tech@users.noreply.github.com>
2021-03-13 10:04:47 -0600

    Added transitions and animations
    I also organized files

---
+++ js/index.js
@@ -0,0 +1,28 @@
+let scrollTop;
+
+setTimeout(function() {
+    const our_goal_text = document.querySelectorAll("#our-goal");
+
+    for (i = 0; i < our_goal_text.length; i++) {
+        our_goal_text[i].classList.add("is-active");
+    }
+}, 1000);
+
+window.addEventListener("scroll", function (e) {
+    scrollTop = window.pageYOffset !== undefined ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+    if (scrollTop < 950 && scrollTop > 800) {
+        const how_to_use_text = document.querySelectorAll("#how-to-use");
+
+        for (i = 0; i < how_to_use_text.length; i++) {
+            how_to_use_text[i].classList.add("is-active");
+        }
+    }
+    else if (scrollTop <= 1878 && scrollTop > 1778) {
+        const about_us_text = document.querySelectorAll("#about-us");
+
+        for (i = 0; i < about_us_text.length; i++) {
+            about_us_text[i].classList.add("is-active");
+        }
+    }
+  });
---
+++ js/transition.js
@@ -0,0 +1,25 @@
+window.onload = () => {
+    const transition = document.querySelector(".transition");
+    const nav_links = document.querySelectorAll(".topnav a")
+
+    // Stop the transition
+    setTimeout (() => {
+        transition.classList.remove("is-active")
+    }, 500);
+
+    // Get all the nav links
+    for (i = 0; i < nav_links.length; i++) {
+        const link = nav_links[i];
+
+        // Add transition to the link
+        link.addEventListener("click", e => {
+            e.preventDefault();
+            let link_url = e.target.href
+            transition.classList.add("is-active")
+
+            setTimeout(() => {
+                window.location.href = link_url;
+            }, 500);
+        });
+    }
+}
