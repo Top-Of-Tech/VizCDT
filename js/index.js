933f70edef16c7a77221bef14c08abe40f841518
Top-Of-Tech <67748521+Top-Of-Tech@users.noreply.github.com>
2021-03-13 12:13:24 -0600

    Made animations trigger sooner

--- js/index.js
+++ js/index.js
@@ -11,14 +11,14 @@ setTimeout(function() {
 window.addEventListener("scroll", function (e) {
     scrollTop = window.pageYOffset !== undefined ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
     console.log(scrollTop);
-    if (scrollTop < 950 && scrollTop > 800) {
+    if (scrollTop < 950 && scrollTop > 500) {
         const how_to_use_text = document.querySelectorAll("#how-to-use");
 
         for (i = 0; i < how_to_use_text.length; i++) {
             how_to_use_text[i].classList.add("is-active");
         }
     }
-    else if (scrollTop <= 1878 && scrollTop > 1778) {
+    else if (scrollTop <= 1878 && scrollTop > 1478) {
         const about_us_text = document.querySelectorAll("#about-us");
 
         for (i = 0; i < about_us_text.length; i++) {
