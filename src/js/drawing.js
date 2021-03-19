9581ba6325eb2f2f1fd210d5fd28ac5462043cc0
Top-Of-Tech <67748521+Top-Of-Tech@users.noreply.github.com>
2021-03-19 15:26:01 -0500

    Added drawing tool

---
+++ src/js/drawing.js
@@ -0,0 +1,57 @@
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+let color = "#000000";
+let width = 10;
+let drawing = false;
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight - 150;
+}
+
+function change_color(element) {
+    color = element;
+}
+
+function change_pen_size() {
+    width = document.querySelector(".pen-size").value;
+}
+
+function change_custom_color() {
+    color = document.querySelector(".custom-color").value;
+}
+
+function clear() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+resizeCanvas();
+
+window.addEventListener("resize", function(e) {
+    drawing = true;
+    resizeCanvas();
+});
+
+window.addEventListener("mousemove" , function(e) {
+    if (drawing == true) {
+        ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width;
+        ctx.lineCap = "round";
+        ctx.lineJoin = "round";
+        ctx.stroke();
+    }
+});
+
+window.addEventListener("mousedown" , function(e) {
+    drawing = true;
+    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    ctx.beginPath();
+});
+
+window.addEventListener("mouseup" , function(e) {
+    drawing = false;
+    ctx.closePath();
+});
+
+document.querySelector(".clear").onclick = clear;
