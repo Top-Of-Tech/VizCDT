e16fce1d9618e36789850ca29537e506c1cc8543
Top-Of-Tech <67748521+Top-Of-Tech@users.noreply.github.com>
2021-03-24 08:29:24 -0500

    Allows tabs to be put in

--- src/js/codeeditor.js
+++ src/js/codeeditor.js
@@ -2,6 +2,19 @@ const button = document.querySelector(".run");
 const editor = document.querySelector(".editor");
 const output = document.querySelector(".output");
 
+var textareas = document.getElementsByTagName('textarea');
+var count = textareas.length;
+for(var i=0;i<count;i++){
+    textareas[i].onkeydown = function(e){
+        if(e.key=="Tab"){
+            e.preventDefault();
+            var s = this.selectionStart;
+            this.value = this.value.substring(0,this.selectionStart) + this.value.substring(this.selectionEnd);
+            this.selectionEnd = s+1; 
+        }
+    }
+}
+
 function color_code() {
 	
 }
@@ -26,5 +39,20 @@ async function run() {
 	output.textContent = json_data["output"];
 	
 }
+
+editor.addEventListener('keydown', function(e) {
+	if (e.key == 'Tab') {
+	  e.preventDefault();
+	  var start = this.selectionStart;
+	  var end = this.selectionEnd;
+  
+	  // set textarea value to: text before caret + tab + text after caret
+	  this.value = this.value.substring(0, start) +
+		"\t" + this.value.substring(end);
+  
+	 this.selectionStart =
+	this.selectionEnd = start + 1;
+	}
+  });
 
 button.onclick = run;
